d724e48ad69603d7c50645d18c6295b48e158cf4
Keith To <keithtkto@gmail.com>
2016-01-29 00:01:45 -0800

    add point tracking component, game initial model ready

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 
 // global var
-var round = 0,
+var redTeam = 0,
+    blueTeam = 0,
+    round = 0,
+    currentPlayer = "blueSM" //"blueFA", "redSM","redFA" //SM = spymaster FA = field agent
     board = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
-    blueFirst = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
+    blueFirst = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
                 'red', 'red', 'red', 'red', 'red', 'red', 'red',
-                'white', 'white', 'white', 'white', 'white', 'white', 'white',
+                'white', 'white', 'white', 'white', 'white', 'white',
                 'white', 'white', 'black'],
     redFirst = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
-                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
-                'white', 'white', 'white', 'white', 'white', 'white', 'white',
+                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
+                'white', 'white', 'white', 'white', 'white', 'white',
                 'white', 'white', 'black'];
+
+
+
+
+/* Model */
+
+/* create game board */
+
 
 
 // 1. Random word generator and remove the selected words from word list
 // 2. change board into arrays of object {color, state, word}
 // 3. add board state (true = clicked/false = not clicked)
 // 4. add color to board depending on round, even = blue first / odd = red first
+
+function setBoard() {}
+
 for ( var i = 0; i < 25; i++ ) {
   var numWord = Math.floor( Math.random() * wordArray.length ),
       numColor = Math.floor( Math.random() * blueFirst.length );
@@ -28,9 +42,25 @@ for ( var i = 0; i < 25; i++ ) {
   if (round % 2 === 0) {
     board[i].color = blueFirst[numColor];
     blueFirst.splice( (numColor), 1 );
+    blueTeam = 9;
+    redTeam = 8;
   } else {
     board[i].color = redFirst[numColor];
     redFirst.splice( (numColor), 1 );
+    redTeam = 9;
+    blueTeam = 8;
   };
 
 };
+
+
+
+
+/* Behavior */
+
+
+
+/* Render */
+
+/* Player Interaction */
+
