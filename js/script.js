7f30f3a80c1f98562e6ce286f0622b2e8d90e79d
Keith To <keithtkto@gmail.com>
2016-01-29 16:29:46 -0800

    fix bug on setting color, add win & searchBlack function

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
 var redSleeperAgents = 0,
     blueSleeperAgents = 0,
     round = 0,
-    winner = 'blue', //'red' is a user input, default blue
-    currentPlayer = "blueSM", //"blueFA", "redSM","redFA" //SM = spymaster FA = field agent
+    winner = '', //'blue' 'red' is a user input, default blue
+    currentPlayer = "", //"blueFA", "redSM","redFA" //SM = spymaster FA = field agent
     latestStartingTeam = "red", //vs "red"
     board = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
     blueFirst = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
-                'red', 'red', 'red', 'red', 'red', 'red', 'red',
+                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
                 'white', 'white', 'white', 'white', 'white', 'white',
                 'white', 'white', 'black'],
     redFirst = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
@@ -26,63 +26,58 @@ var redSleeperAgents = 0,
 
 
 // 1. Random word generator and remove the selected words from word list
-// 2. change board into arrays of object {color, state, word}
-// 3. add board state (true = clicked/false = not clicked)
-// 4. add color to board depending on round, even = blue first / odd = red first
+// 2. change board into arrays of object {color, word}
+// 3. add color to board depending on round, even = blue first / odd = red first
 
+//okay
 function setBoard() {}
 
 for ( var i = 0; i < 25; i++ ) {
   var numWord = Math.floor( Math.random() * wordArray.length );
-
+  // console.log(numWord);
   board[i].word = wordArray[numWord];
+  // console.log(board[i])
   wordArray.splice( (numWord), 1 );
 
   board[i].state = false;
+};
 
+
+
+// generate random state
+// add board state (true = clicked/false = not clicked)
+
+//okay
+function setColor() {}
   if (latestStartingTeam === "red") {
+    for ( var i = 0; i < 25; i++ ) {
       var numColor = Math.floor( Math.random() * blueFirst.length );
+      // console.log(numColor);
       board[i].color = blueFirst[numColor];
       blueFirst.splice( (numColor), 1 );
       blueSleeperAgents = 9;
       redSleeperAgents = 8;
-      latestStartingTeam = "blue";
+      }
+    latestStartingTeam = "blue";
     } else {
+    for ( var i = 0; i < 25; i++ ) {
       var numColor = Math.floor( Math.random() * redFirst.length );
+      // console.log(numColor);
       board[i].color = redFirst[numColor];
       redFirst.splice( (numColor), 1 );
       redSleeperAgents = 9;
       blueSleeperAgents = 8;
-      latestStartingTeam = "red";
     };
-};
+  latestStartingTeam = "red";
+  };
+
 
 /* Behavior */
-
-// 1. turns of the game  (day1)blueSM --> blueFA --> redSM -->redFA -->(day2) blueSM
-
-switch(currentPlayer) {
-    case "blueSM":
-      currentPlayer = "blueFA";
-      break;
-    case "blueFA":
-      currentPlayer = "redSM";
-      break;
-    case "redSM":
-      currentPlayer = "redFA";
-      break;
-    case "redFA":
-      currentPlayer = "blueSM";
-      break;
-    default:
-      console.log("error on current player");
-};
-
-// 2. MOVES
+// 1. MOVES
 //    a. Spymaster move
 
+// need work
 function moves() {};
-
 switch(currentPlayer) {
   case "blueSM":
       return = #blueteam word/num input#;
@@ -103,22 +98,63 @@ switch(currentPlayer) {
 
 //    b. field agent move
 
+// 2. selecting next player  (day1)blueSM --> blueFA --> redSM -->redFA -->(day2) blueSM
+
+//okay
+function nextPlayer() {}
+  switch(currentPlayer) {
+      case "blueSM":
+        currentPlayer = "blueFA";
+        break;
+      case "blueFA":
+        currentPlayer = "redSM";
+        break;
+      case "redSM":
+        currentPlayer = "redFA";
+        break;
+      case "redFA":
+        currentPlayer = "blueSM";
+        break;
+      default:
+        console.log("error on current player");
+  };
+
 
 
 
 
 // win condition
-if (blueSleeperAgents === 0) {
-  winner = 'blue team';
-} else if (redSleeperAgents === 0) {
-  winner = 'red team';
-};
 
-//Immediate loss (8-ball) condition
+//okay
+function win() {}
+  if (blueSleeperAgents === 0) {
+    winner = 'blue team';
+  } else if (redSleeperAgents === 0) {
+    winner = 'red team';
+  };
 
-if (currentPlayer === #"blueFA" and select "black"#) {
+
+
+//Immediate loss (8-ball) condition and where is teh 8ball
+// Where is the 8ball
+// for ( var e = 0; 2 < board.length; e++) { if (board[e].color === "black") {console.log(e)}; };
+// VM1218:2 11
+
+//okay
+function searchBlack(state, board) {}
+    for (var i=0; i < 25; i++) {
+        if ( board[i].color === "black") {
+            console.log(i)
+            return i;
+        }
+    }
+
+// loss condition
+
+// need work
+if (currentPlayer === "blueFA" && board[11].color === "black" && board[11].state === true) {
   winner = "red team";
-} else if (currentPlayer === #"redFA" and select "black"#) {
+} else if (currentPlayer === "redFA" && board[11].color === "black" && board[11].state === true) {
   winner = "blue team";
 }
 
