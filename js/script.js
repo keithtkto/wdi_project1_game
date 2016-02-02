04ecea473e1db963047c6ba246746f67ed23d984
Keith To <keithtkto@gmail.com>
2016-02-02 15:26:39 -0800

    refactor code

--- js/script.js
+++ js/script.js
@@ -6,27 +6,35 @@ console.log("CAPCOM, We're GO for Powered Descent.");
 
 
 // global var
-var redSleeperAgents = 0,
+var redSleeperAgents  = 0,
     blueSleeperAgents = 0,
-    round = 0,
-    winner = '', //'blue' 'red' is a user input, default blue
-    currentPlayer = "", //"blueFA", "redSM","redFA" //SM = spymaster FA = field agent
-    currentTeam = "blue", //vs "red"
-    board = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},
-              {},{},{},{},{},{},{},{}],
-    blueStock = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
-                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
-                '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5',
-                '#fff2e5', 'black'],
-    redStock = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
-                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red',
-                '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5', '#fff2e5',
-                '#fff2e5', 'black'],
-    blueFirst = [],
-    redFirst = [],
+    round             = 0,
+    winner            = "", //'blue' 'red' is a user input, default blue
+    currentPlayer     = "", //"blueFA", "redSM","redFA" //SM = spymaster FA = field agent
+    currentTeam       = "", //vs "red"
+    board             = [
+      {},{},{},{},{},
+      {},{},{},{},{},
+      {},{},{},{},{},
+      {},{},{},{},{},
+      {},{},{},{},{}
+    ],
+    blueFirst,
+    redFirst,
+    blueStock,
+    redStock,
     clueValue,
     indexBlack,
     indexClicked;
+
+blueStock = Array(25).fill("blue", 0, 9)
+                     .fill("red", 9, 17)
+                     .fill("#fff2e5", 17, 24)
+                     .fill("black", 24, 25);
+redStock  = Array(25).fill("red", 0, 9)
+                     .fill("blue", 9, 17)
+                     .fill("#fff2e5", 17, 24)
+                     .fill("black", 24, 25);
 
 // jQuery Variable
 /* Model */
@@ -37,26 +45,19 @@ var redSleeperAgents = 0,
 
 /* game operation */
 function startGame() {
-
-//adding properties to board[]
-setBoard();
-setColor();
-renderFA();
-whereBlackAt();
-// adding words in each box
-wordInBox();
-disableSubmit()
-pass()
-// blue spymaster start
+  //adding properties to board[]
+  setBoard();
+  setColor();
+  renderFA();
+  // adding words in each box
+  wordInBox();
+  disableSubmit()
+  pass()
+  // blue spymaster start
 }; // end of startGame()
 
 
 // 1. MOVES
-
-
-function firstMove() {
-
-}
 
 function blueSMmove() {
   renderRemainingSA()
@@ -64,7 +65,6 @@ function blueSMmove() {
   renderSM();
   eventOffSM()
   blueSubmit();
-
 };
 
 function blueFAmove() {
@@ -89,9 +89,6 @@ function redFAmove() {
   clickOffFA();
 };
 
-
-
-
 // 1. Random word generator and remove the selected words from word list
 // 2. change board into arrays of object {color, word}
 // 3. add color to board depending on round, even = blue first / odd = red first
@@ -101,26 +98,22 @@ function drawBoard() {
   for (i= 0; i < 25; i++) {
     var $word_box = $("<div class='word_box'><p class='words'></p></div>");
     $word_box.appendTo('.middlebox');
-    $('div>p:last').attr("id", "box" + i)
-    $("#box"+i).text("???")
-  };
-};
+    $('div > p:last').attr("id", "box" + i);
+    $("#box"+i).text("???");
+  }
+}
 
 drawBoard()
 
 //okay
 function setBoard() {
-
-  for ( var i = 0; i < 25; i++ ) {
+  for (var i = 0; i < 25; i++) {
     var numWord = Math.floor( Math.random() * wordArray.length );
-    // console.log(numWord);
-    board[i].word = wordArray[numWord];
-    // console.log(board[i])
-    wordArray.splice( (numWord), 1 );
 
-    board[i].state = false;
-  };
-};
+    board[i].word    = wordArray.splice(numWord, 1)[0];
+    board[i].clicked = false;
+  }
+}
 
 // generate random state
 // board state (true = clicked/false = not clicked)
@@ -128,45 +121,32 @@ function setBoard() {
 
 //okay
 function setColor() {
-  blueFirst += blueStock;
-  blueFirst = blueFirst.split(",")
-  redFirst += redStock;
-  redFirst = redFirst.split(",")
+  blueFirst = blueStock.slice(); // COPY the arrays, don't reference them directly
+  redFirst  = redStock.slice();
+
   if (currentTeam === "blue") {
-    for ( var i = 0; i < 25; i++ ) {
-      var numColor = Math.floor( Math.random() * blueFirst.length );
-      // console.log(numColor);
-      board[i].color = blueFirst[numColor];
-      blueFirst.splice( (numColor), 1 );
+    for (var i = 0; i < 25; i++) {
+      var numColor = Math.floor(Math.random() * blueFirst.length);
+      board[i].color = blueFirst.splice(numColor, 1)[0];
+      if (board[i].color === "black") indexBlack = i;
     }
-  // blueFirst = blueStock;
-    // latestStartingTeam = "blue";
   } else if (currentTeam === "red") {
     for ( var i = 0; i < 25; i++ ) {
-      var numColor = Math.floor( Math.random() * redFirst.length );
-      // console.log(numColor);
-      board[i].color = redFirst[numColor];
-      redFirst.splice( (numColor), 1 );
-      // latestStartingTeam = "red"
-    };
-  // redFirst = redStock;
-  };
+      var numColor = Math.floor(Math.random() * redFirst.length);
+      board[i].color = redFirst.splice(numColor, 1)[0];
+      if (board[i].color === "black") indexBlack = i;
+    }
+  }
 }
-
 
   // Putting word into correlating boxes
 function wordInBox() {
   for (i= 0; i < 25; i++) {
     $("#box"+i).text(board[i].word);
-  };
-};
-
+  }
+}
 
 /* Behavior */
-
-
-
-
 
 // win condition
 
@@ -182,28 +162,6 @@ function win() {
   startGame()
 }
 
-
-//Immediate loss (8-ball) condition and where is teh 8ball
-// Where is the 8ball
-// for ( var e = 0; 2 < board.length; e++) { if (board[e].color === "black") {console.log(e)}; };
-// VM1218:2 11
-
-//okay
-function whereBlackAt() {
-    for (var i=0; i < 25; i++) {
-        if ( board[i].color === "black") {
-            console.log(i)
-            indexBlack = i;
-            // return blackLocation;
-        };
-    };
-};
-// loss condition
-
-// need work (need to connect with click)
-
-
-
 //ending a round (activated around each round)
 //nested within activate
 function endRound() {
@@ -213,46 +171,42 @@ function endRound() {
 }
 
 function eightBall() {
-    if (currentPlayer === "blueFA" && board[indexBlack].state === true) {
-      winner = "red team";
-      console.log("red team wins")
-
-    } else if (currentPlayer === "redFA" && board[indexBlack].state === true) {
-      winner = "blue team";
-      console.log("blue team wins")
-
-  };
-};
+  if (currentPlayer === "blueFA" && board[indexBlack].clicked === true) {
+    winner = "red team";
+    console.log("red team wins")
+  } else if (currentPlayer === "redFA" && board[indexBlack].clicked === true) {
+    winner = "blue team";
+    console.log("blue team wins")
+  }
+}
 
 //2. selecting wrong sleeper agent // if clicked box != same color or white
 // prob need to add (THIS) === clicked_word in condition
   //need animation for this
 function wrongAgent() {
-  if (currentPlayer === "blueFA" && board[indexClicked].color !== "blue" && board[indexClicked].state === true) {
-      console.log("blue team round ends", board[indexClicked])
-      redSMmove();
-  } else if (currentPlayer === "redFA" && board[indexClicked].color !== "red" && board[indexClicked].state === true) {
-      console.log("red team round ends", board[indexClicked])
-      blueSMmove();
+  if (currentPlayer === "blueFA" && board[indexClicked].color !== "blue" && board[indexClicked].clicked === true) {
+    console.log("blue team round ends", board[indexClicked])
+    redSMmove();
+  } else if (currentPlayer === "redFA" && board[indexClicked].color !== "red" && board[indexClicked].clicked === true) {
+    console.log("red team round ends", board[indexClicked])
+    blueSMmove();
   };
 };
 
 //3. ending by running out of moves
     //need animation for this
 function correctAgent() {
-    clueValue--
-    if (clueValue === 0 && currentPlayer === "blueFA") {
-      console.log(clueValue);
-      console.log("blue got all the correct agent activated");
-      redSMmove();
-    } else if (clueValue === 0 && currentPlayer === "blueFA") {
-      console.log(clueValue);
-      console.log("red got the correct agent activated");
-      blueSMmove();
-    };
+  clueValue--
+  if (clueValue === 0 && currentPlayer === "blueFA") {
+    console.log(clueValue);
+    console.log("blue got all the correct agent activated");
+    redSMmove();
+  } else if (clueValue === 0 && currentPlayer === "blueFA") {
+    console.log(clueValue);
+    console.log("red got the correct agent activated");
+    blueSMmove();
+  };
 };
-
-
 
 /*
   if (currentPlayer === "blueFA" && #box selected# !== "blue" ) {
@@ -274,12 +228,12 @@ function renderFA() {
   for (var i =0; i < 25; i++) {
     $('.word_box').eq(i).css('background', '#fff2e5');
     $("#box"+i).css('background', '#fff2e5');
-    if (board[i].state === true) {
+    if (board[i].clicked === true) {
       $('.word_box').eq(i).css('background', board[i].color);
-      $("#box"+i).text("UNAVAIBLE").css('background', board[i].color);
+      $("#box"+i).text("UNAVAILABLE").css('background', board[i].color);
     }
-  };
-};
+  }
+}
 
 
 // render for Spymaster
@@ -287,11 +241,11 @@ function renderFA() {
 function renderSM() {
   for (var i =0; i < 25; i++) {
     $('.word_box').eq(i).css('background', board[i].color)
-    if (board[i].state === true) {
-      $("#box"+i).text("UNAVAIBLE").css('background', board[i].color);
+    if (board[i].clicked === true) {
+      $("#box"+i).text("UNAVAILABLE").css('background', board[i].color);
     }
-  };
-};
+  }
+}
 
 // render for amount of sleeper agent
 
@@ -299,10 +253,10 @@ function renderRemainingSA() {
   blueSleeperAgents = 0;
   redSleeperAgents = 0;
   for (var i =0; i < 25; i++) {
-    if (board[i].color === "blue" && board[i].state === false) {
+    if (board[i].color === "blue" && board[i].clicked === false) {
       blueSleeperAgents++
-    };
-    if (board[i].color === "red" && board[i].state === false) {
+    }
+    if (board[i].color === "red" && board[i].clicked === false) {
       redSleeperAgents++
     }
   }
@@ -317,39 +271,36 @@ function renderRemainingSA() {
 
 function clickOnFA() {
   for (i= 0; i < 25; i++) {
-    if (board[i].state === false) {
-      $("#box"+i).on("click", activate );
-      $('.word_box').eq(i).on("click", activate );
-    };
-  };
-};
+    if (board[i].clicked === false) {
+      $("#box"+i).on("click", activate);
+      $('.word_box').eq(i).on("click", activate);
+    }
+  }
+}
 
 //In field agent round, remove event listener if its already been activated (state:true)
 
 function clickOffFA() {
   for (var i =0; i < 25; i++) {
-    if (board[i].state === true) {
+    if (board[i].clicked === true) {
       $("#box"+i).off()
       $('.word_box').eq(i).off()
     }
   }
-};
-
-
-
+}
 
 // turn board state from false to true when clicked
 function activate() {
   console.log( $( this ).text() );
   for (var i = 0; i <25 ; i++) {
     if ( board[i].word === $(this).text() ) {
-        indexClicked = i;
-        board[i].state = true;
+      indexClicked = i;
+      board[i].clicked = true;
     };
     if ($(this).text() === $(".words").eq(i).text() && board[i].word === $(this).text() ) {
-        board[i].state = true;
+      board[i].clicked = true;
     };
-    console.log(board[i].word, board[i].state, board[i].color);
+    console.log(board[i].word, board[i].clicked, board[i].color);
   };
   console.log("activated")
   renderFA()
@@ -365,10 +316,33 @@ function eventOffSM() {
   for (var i =0; i < 25; i++) {
     $("#box"+i).off()
     $('.word_box').eq(i).off()
-  };
-};
+  }
+}
 
 // submit no refresh
+
+
+// function playerSubmit(color) {
+//   var colorInitial = color.charAt(0).toUpperCase();
+
+//   currentPlayer = color + "SM"
+//   $("#send-button" + colorInitial).attr("disabled", false);
+//   $("select").eq(0).attr("disabled", false);
+//   $("#textarea" + colorInitial).attr("disabled", false);
+//   $("#textarea" + colorInitial).attr("placeholder", "Uplink enabled...Enter your single word clue here")
+//   $("#send-button" + colorInitial).attr('value', 'TRANSMIT');
+//   $( "#send-button" + colorInitial ).click(function(evt) {
+//     evt.preventDefault();
+
+//     clueValue = $('select')[0].value
+//     var newClue = $('#textarea' + colorInitial).val() + ' ' + clueValue;
+//     $li = $("<li id='clue'" + colorInitial + ">").text(newClue);
+//     $( "#clue" + colorInitial + color.slice(1) ).append( $li );
+
+//     currentPlayer = color + "FA";
+//     color === "blue" ? blueFAmove() : redFAmove();
+//   });
+// };
 
 function blueSubmit() {
   currentPlayer = "blueSM"
@@ -377,17 +351,17 @@ function blueSubmit() {
   $("#textareaB").attr("disabled", false);
   $("#textareaB").attr("placeholder", "Uplink enabled...Enter your single word clue here")
   $("#send-buttonB").attr('value', 'TRANSMIT');
-  $( "#send-buttonB" ).click( function( evt ) {
+  $( "#send-buttonB" ).click(function(evt) {
     evt.preventDefault();
+
     clueValue = $('select')[0].value
     var newClue = $('#textareaB').val() + ' ' + clueValue;
     $li = $("<li id='clueB'>").text(newClue);
     $( "#clueBlue" ).append( $li );
 
-    currentPlayer = "blueFA"
-    blueFAmove()
-
-    });
+    currentPlayer = "blueFA";
+    blueFAmove();
+  });
 };
 
 
@@ -398,15 +372,15 @@ function redSubmit() {
   $("#textareaR").attr("disabled", false);
   $("#textareaR").attr("placeholder", "Uplink enabled...Enter your single word clue here")
   $("#send-buttonR").attr('value', 'TRANSMIT');
-  $( "#send-buttonR" ).click(function( evt ) {
+  $( "#send-buttonR" ).click(function(evt) {
     evt.preventDefault();
     clueValue = $('select')[1].value
     var newClue = $('#textareaR').val() + ' ' + clueValue;
     $li = $("<li id='clueR'>").text(newClue);
     $( "#clueRed" ).append( $li );
 
-    currentPlayer = "redFA"
-    redFAmove()
+    currentPlayer = "redFA";
+    redFAmove();
   });
 };
 
@@ -440,24 +414,22 @@ function pass() {
 
 $('#bluestart').on('click',function(){
   $("#ss").attr('class', "hidden")
+  currentTeam = "blue";
   startGame();
   blueSMmove();
 });
 
 $('#redstart').on('click',function(){
   $("#ss").attr('class', "hidden")
+  currentTeam = "red";
   startGame();
   redSMmove();
-
 });
-
 
 // restart game
 $("#restartGame").on('click', startGame)
 
-
 //refresh color only (provide warning for players)
-
 $("#refreshColor").on('click', function(){
   console.log("refresh color");
   setColor();
@@ -465,15 +437,8 @@ $("#refreshColor").on('click', function(){
 });
 
 //refresh words only (provide warning for players)
-
 $("#refreshWords").on('click', function() {
   event.preventDefault();
   setBoard();
   wordInBox();
 });
-
-
-
-
-
-
